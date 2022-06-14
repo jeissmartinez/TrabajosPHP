f87ac61bf70652c3e58d61be9cba1fce5d97e98e
Aprendiz sena <ningunapersona7w7@gmail.com>
2022-06-14 16:17:05 -0500

    primera coneccion a la base biblioteca

---
+++ 14-06-22/index.php
@@ -0,0 +1,10 @@
+<?php
+    $NombreServidor="localhost";
+    $NombreUsuario="root";
+    $Contraseña="";
+    $BaseDeDatos="biblioteca";
+    $Contexion="mysqli_connect($NombreServidor,$NombreUsuario,$Contraseña,$BaseDeDatos)";
+
+    echo "Se realizo bien la conexion";
+
+?>
